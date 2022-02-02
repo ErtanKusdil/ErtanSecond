230557649562adc7e777ccf15fa1cd86df2dc8f4
ErtanKusdil <ertankusdil@hotmail.com>
2022-02-01 21:54:37 -0500

    Initial commit

---
+++ src/day02_print_statements/TellMeAboutYourSelf.java
@@ -0,0 +1,15 @@
+package day02_print_statements;
+
+public class TellMeAboutYourSelf{
+
+    public static void main(String [] args) {
+
+
+        System.out.println("Ertan");
+        System.out.println("New Jersey");
+        System.out.println("Watching");
+        System.out.println("1000");
+
+
+    }
+}
